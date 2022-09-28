b82b7c8faab9b5d8e9d3a750cd277624092ab0b7
Комарова Ксения <xenia.komarr@gmail.com>
2022-09-28 23:27:29 +0300

    Бот работает

--- kittybot.py
+++ kittybot.py
@@ -1,7 +1,9 @@
-import telegram
+from telegram import Bot
 
 
 bot = Bot(token='5519495382:AAEEDWKhxsOabr8S40p5z8A5sw22sjNmFgw')
 
 chat_id = 988057934
 text = 'Вам телеграмма!'
+
+bot.send_message(chat_id, text)
